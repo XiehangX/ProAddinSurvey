e8045848ad8d9ebb208574091f957b467607a24b
XieHangX <xiehang2005@gmail.com>
2021-10-11 13:33:55 +0800

    Add ExcelDataReader

---
+++ ProAddinSurvey/Common/ExcelHelper.cs
@@ -0,0 +1,140 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProAddinSurvey.Common
+{
+    public static class ExcelHelper
+    {
+
+        public static DataSet LoadDataset(string filePath, int headerRow)
+        {
+            using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    var configuration = new ExcelDataSetConfiguration
+                    {
+                        UseColumnDataType = true,
+
+                        ConfigureDataTable = tableReader => new ExcelDataTableConfiguration
+                        {
+                            FilterRow = rowReader => rowReader.Depth > headerRow - 1,
+                            UseHeaderRow = true,
+                            ReadHeaderRow = (rowReader) => {
+                                for (int i = 1; i < headerRow; i++)
+                                {
+                                    rowReader.Read();
+                                }
+                            },
+                        }
+                    };
+                    return reader.AsDataSet(configuration);
+                }
+            }
+        }
+
+        public static DataTable LoadTableFirst(string filePath, int headerRow)
+        {
+            var ds = LoadDataset(filePath, headerRow);
+            return ds?.Tables[0];
+        }
+
+        public static List<T> DataTableToList<T>(this DataTable table) where T : class, new()
+        {
+            try
+            {
+                List<T> list = new List<T>();
+
+                foreach (var row in table.AsEnumerable())
+                {
+                    T obj = new T();
+
+                    foreach (var prop in obj.GetType().GetProperties())
+                    {
+                        try
+                        {
+                            PropertyInfo propertyInfo = obj.GetType().GetProperty(prop.Name);
+                            propertyInfo.SetValue(obj, Convert.ChangeType(row[prop.Name], propertyInfo.PropertyType), null);
+                        }
+                        catch
+                        {
+                            continue;
+                        }
+                    }
+
+                    list.Add(obj);
+                }
+
+                return list;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        private static readonly IDictionary<Type, ICollection<PropertyInfo>> _Properties =
+            new Dictionary<Type, ICollection<PropertyInfo>>();
+
+        /// <summary>
+        /// Converts a DataTable to a list with generic objects
+        /// </summary>
+        /// <typeparam name="T">Generic object</typeparam>
+        /// <param name="table">DataTable</param>
+        /// <returns>List with generic objects</returns>
+        public static IEnumerable<T> DataTableToListX<T>(this DataTable table) where T : class, new()
+        {
+            try
+            {
+                var objType = typeof(T);
+                ICollection<PropertyInfo> properties;
+
+                lock (_Properties)
+                {
+                    if (!_Properties.TryGetValue(objType, out properties))
+                    {
+                        properties = objType.GetProperties().Where(property => property.CanWrite).ToList();
+                        _Properties.Add(objType, properties);
+                    }
+                }
+
+                var list = new List<T>(table.Rows.Count);
+
+                foreach (var row in table.AsEnumerable().Skip(1))
+                {
+                    var obj = new T();
+
+                    foreach (var prop in properties)
+                    {
+                        try
+                        {
+                            var propType = Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType;
+                            var safeValue = row[prop.Name] == null ? null : Convert.ChangeType(row[prop.Name], propType);
+
+                            prop.SetValue(obj, safeValue, null);
+                        }
+                        catch
+                        {
+                            continue;
+                        }
+                    }
+
+                    list.Add(obj);
+                }
+
+                return list;
+            }
+            catch
+            {
+                return Enumerable.Empty<T>();
+            }
+        }
+
+    }
+}
